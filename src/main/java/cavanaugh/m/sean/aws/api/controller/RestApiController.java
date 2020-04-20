aa28fcc0833a3b248c82316b664eda1af07b240a
Sean Cavanaugh <cavanaughmsean@gmail.com>
2020-04-19 20:47:41 -0500

    Adding in environment to hello world messaging

--- src/main/java/cavanaugh/m/sean/aws/api/controller/RestApiController.java
+++ src/main/java/cavanaugh/m/sean/aws/api/controller/RestApiController.java
@@ -1,16 +1,28 @@
 package cavanaugh.m.sean.aws.api.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
+import static java.lang.String.join;
+import static java.util.Arrays.stream;
+import static java.util.stream.Collectors.joining;
 import static org.springframework.http.HttpStatus.OK;
 
 @RestController
 public class RestApiController {
 
+    @Autowired
+    private Environment environment;
+
     @GetMapping("/")
     public ResponseEntity helloWorld() {
-        return new ResponseEntity("Hello World", OK);
+        String profiles = join(" ", environment.getActiveProfiles());
+        return new ResponseEntity("Hello World from profiles: " + profiles, OK);
     }
 }
--- src/test/java/cavanaugh/m/sean/aws/api/controller/RestApiControllerTest.java
+++ src/test/java/cavanaugh/m/sean/aws/api/controller/RestApiControllerTest.java
@@ -2,25 +2,34 @@ package cavanaugh.m.sean.aws.api.controller;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.core.env.Environment;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import org.springframework.test.web.servlet.setup.StandaloneMockMvcBuilder;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.setup.MockMvcBuilders.standaloneSetup;
 
+@ExtendWith(MockitoExtension.class)
 class RestApiControllerTest {
 
     private MockMvc mockMvc;
+    @InjectMocks
     private RestApiController controller;
+    @Mock
+    private Environment environment;
 
     @BeforeEach
-    void setUp() {
-        controller = new RestApiController();
-        mockMvc = MockMvcBuilders.standaloneSetup(controller).build();
-
+    public void setUp() {
+        mockMvc = standaloneSetup(controller).build();
+        when(environment.getActiveProfiles()).thenReturn(new String[]{"Pilot"});
     }
 
     @Test
@@ -30,8 +39,14 @@ class RestApiControllerTest {
     }
 
     @Test
-    public void helloWorld_ReturnsHelloWorld() throws Exception {
-        assertEquals("Hello World", controller.helloWorld().getBody());
+    public void helloWorld_ReturnsHelloWorld_WithEnvironment() throws Exception {
+        assertEquals("Hello World from profiles: Pilot", controller.helloWorld().getBody());
+    }
+
+    @Test
+    public void helloWorld_ReturnsHelloWorld_WithMultipleEnvironments() throws Exception {
+        when(environment.getActiveProfiles()).thenReturn(new String[]{"Test, Pilot"});
+        assertEquals("Hello World from profiles: Test, Pilot", controller.helloWorld().getBody());
     }
 
 }
